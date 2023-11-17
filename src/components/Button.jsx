3f74cae7a48ceabec57b9709a07a5c6639877101
solux <lee.yeji0327@gmail.com>
2023-11-18 01:22:52 +0900

    [feat] button 컴포넌트 생성

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
+import arrow from '../images/arrow.svg';
 
-const Button = () => {
+const Btn = styled.div `
+    background-color: #FF528B;
+    cursor: pointer;
+    font-family: 'Dongle-regular';
+    font-weight: 600;
+    font-size: 3.5rem;
+    color: white;
+    width: 300px;
+    height: 120px;
+    border-radius: 25px;
+    line-height: 120px;
+    display: flex;
+    align-items: center;
+`
+const Txt = styled.div`
+    margin: auto;
+    display: flex;
+    align-items: center;
+`
+
+const Button = (props) => {
+    const { onClick, title } = props;
     return (
-        <div>
-            
-        </div>
+        <Btn onClick={onClick}>
+            <Txt>
+            {title || "버튼"}
+            <img src={arrow} style={{width:"80px", marginLeft:"30px"}}/>
+            </Txt>
+        </Btn>
     );
 };
 
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
+import biglogo from '../images/songyee.png';
+import Button from '../components/Button';
+
+const Wrapper = styled.div`
+    background-color: #FFE27C;
+    width: 100vw;
+    height: 100vh;
+    text-align: center;
+`;
+const Txt = styled.div`
+    font-family: 'Dongle-regular';
+    font-weight: 600;
+    font-size: 3.5rem;
+    letter-spacing: 3px;
+    margin-top: -40px;
+`;
+const TxtBtn = styled.div`
+    display: flex;
+    align-items: center;
+`;
 
 const LandingPage = () => {
     return (
-        <div>
-            송이의숲으로 어서오세요
-        </div>
+        <Wrapper>
+            <img src={biglogo} style={{width:"80vw"}}/>
+            <TxtBtn>
+                <Txt>나만의 챌린지 메이트!<br/>송이들과 함께라면 어디든지 갈 수 있어~</Txt>
+                <Button title='시작하기'/>
+            </TxtBtn>
+        </Wrapper>
     );
 };
 
