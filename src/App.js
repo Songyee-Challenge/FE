a75e243e6e9812f78532cc93af74fdec18304739
solux <lee.yeji0327@gmail.com>
2023-11-17 22:19:59 +0900

    [add] font dongle globalstyle로 일괄 적용

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
 import LandingPage from './pages/LandingPage';
+import Topbar from './components/Topbar';
+import styled from 'styled-components';
+import { createGlobalStyle } from "styled-components";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path='/' element={<LandingPage/>}/>
-      </Routes>
+    <GlobalStyle />
+      <Topbar/>
+      <Page>
+        <Routes>
+          <Route path='/' element={<LandingPage/>}/>
+        </Routes>
+      </Page>
     </>
   );
 }
 
 export default App;
+
+const Page = styled.div`
+  margin-top: 15vh;
+`;
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Dongle-regular', sans-serif;;
+  }
+`;
--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import styled from 'styled-components';
+import logo from '../images/logo.png';
+
+const Wrapper = styled.div`
+    width: 100%;
+    height: 15vh;
+    line-height: 15vh;
+    display: flex;
+    align-items: center;
+    padding-left: 2vw;
+    padding-right: 2vw;
+    border-bottom: solid 1px black;
+    position: fixed;
+    top: 0px;
+    background-color: white;
+`;
+const Menu = styled.div`
+
+`;
 
 const Topbar = () => {
     return (
-        <div>
-            
-        </div>
+        <Wrapper>
+            <img src={logo} style={{width:"10vw"}}/>
+            <Menu>송이의 챌린지</Menu>
+            <Menu>송이의 일기장</Menu>
+            <Menu>마이 챌린지</Menu>
+        </Wrapper>
     );
 };
 
