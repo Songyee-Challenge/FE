e996750b51e5789493f6390174391bc3303908b3
solux <lee.yeji0327@gmail.com>
2023-11-17 23:31:32 +0900

    [feat] 경로에 따라 topbar 유무 구현

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import './App.css';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import LandingPage from './pages/LandingPage';
 import Topbar from './components/Topbar';
 import styled from 'styled-components';
 import { createGlobalStyle } from "styled-components";
 import HomePage from './pages/HomePage';
+import { useState } from 'react';
+import InvitePage from './pages/InvitePage';
 
 function App() {
+   const location = useLocation();
+
   return (
     <>
     <GlobalStyle />
-      <Topbar/>
+      <Routes>
+          <Route path='/' element={<LandingPage/>}/>
+          <Route path='/hello' element={<InvitePage/>}/>
+      </Routes>
+      {location.pathname !== '/' && location.pathname !== '/hello' &&<Topbar/>}
       <Page>
         <Routes>
           <Route path='/home' element={<HomePage/>}/>
---
+++ src/pages/InvitePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const InvitePage = () => {
+    return (
+        <div>
+            너굴 : 하이
+        </div>
+    );
+};
+
+export default InvitePage;
