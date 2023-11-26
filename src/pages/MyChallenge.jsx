a0463040f2d9e99a95403fe0231b2f7e54a5bb30
earl9rey <earl9rey@naver.com>
2023-11-26 13:14:51 +0900

    [chore] MChallenge UI수정

--- src/pages/MyChallenge.jsx
+++ src/pages/MyChallenge.jsx
@@ -9,7 +9,7 @@ import profile from "../images/profile.png";
 import editprofile from "../images/editprofile.png";
 import axios from "axios";
 import ProgressBar from "../components/ProgressBar";
-import example from '../images/exampleimage.png';
+import example from "../images/exampleimage.png";
 
 const Wrapper = styled.div`
   margin-left: 3vw;
@@ -112,25 +112,26 @@ const ChDiv = styled.div`
   height: 173px;
   margin-left: 30px;
   margin-right: 80px;
-  margin-top:20px;
-`
+  margin-top: 20px;
+`;
 
 const ChallengeImg = styled.img`
   width: 100%;
   height: 100%;
   object-fit: cover;
+  border-radius: 30px;
 `;
 
 const TextWrapper = styled.div`
   display: flex;
-  font-family:'Pretendard';
+  font-family: "Pretendard";
   flex-direction: column;
   flex: 1;
   align-items: flex-start;
   margin-left: -50px;
   margin-top: 20px;
   white-space: nowrap;
-  color:black;
+  color: black;
 `;
 
 const ChallengeInfo = styled.div`
@@ -142,18 +143,17 @@ const ChallengeInfo = styled.div`
 
 const InfoItem = styled.div`
   display: flex;
-//   justify-content: flex-start;
-  margin-bottom: 20px; /* 간격 조절 */
+  margin-bottom: 20px;
 `;
 
 const InfoLabel = styled.div`
   margin-right: 30%;
   white-space: nowrap;
-  // margin-right: auto; /* 라벨과 값 간 간격 조절 */
+  margin-right: -20px;
 `;
 
 const ShowMissionbtn = styled.button`
-  font-family: 'Pretendard';
+  font-family: "Pretendard";
   border: 1px solid #000000;
   background-color: white;
   color: black;
@@ -169,7 +169,7 @@ const Num = styled.p`
   margin: 0px 30px 5px auto;
   color: black;
   font-weight: bolder;
-  font-family:'Pretendard';
+  font-family: "Pretendard";
 `;
 
 const MyChallenge = () => {
@@ -234,7 +234,7 @@ const MyChallenge = () => {
                     </Container>
                     <Edit src={editprofile} />
                     <input
-                      type="file" // 파일 선택 input으로 변경
+                      type="file"
                       id="fileInput"
                       accept="image/*"
                       onChange={handleFileInputChange}
@@ -258,19 +258,27 @@ const MyChallenge = () => {
                   </More>
                 </Type>
                 <ChallengeBox>
-                  <ChDiv><ChallengeImg src={example} /></ChDiv>
+                  <ChDiv>
+                    <ChallengeImg src={example} />
+                  </ChDiv>
                   <TextWrapper>
-                    <h2 style={{fontSize:'20px', fontWeight: 'bold'}}>KBS 한국어능력시험</h2>
-                      <ChallengeInfo>
-                          <InfoItem>
-                            <InfoLabel>기간: 2023.10.30~2023.11.30</InfoLabel>
-                          </InfoItem>
-                          <InfoItem>
-                            <InfoLabel>진행률: </InfoLabel>
-                            <InfoLabel style={{marginLeft:'-35'}}><ProgressBar/></InfoLabel>
-                          </InfoItem>
-                          <ShowMissionbtn onClick={()=>navigate("/")}>챌린지 보러가기</ShowMissionbtn>
-                      </ChallengeInfo>
+                    <h2 style={{ fontSize: "20px", fontWeight: "bold" }}>
+                      KBS 한국어능력시험
+                    </h2>
+                    <ChallengeInfo>
+                      <InfoItem>
+                        <InfoLabel>기간: 2023.10.30~2023.11.30</InfoLabel>
+                      </InfoItem>
+                      <InfoItem>
+                        <InfoLabel>진행률: </InfoLabel>
+                        <InfoLabel>
+                          <ProgressBar />
+                        </InfoLabel>
+                      </InfoItem>
+                      <ShowMissionbtn onClick={() => navigate("/")}>
+                        챌린지 보러가기
+                      </ShowMissionbtn>
+                    </ChallengeInfo>
                   </TextWrapper>
                   {/* <p>예정된 챌린지가 없습니다.</p> */}
                   <Num>총 {total}개</Num>
